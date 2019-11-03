71bc604fed723f6dfa5978aa987a97cef1864c4d
John Horton <johnharoldhorton@gmail.com>
2019-11-03 16:13:25 -0600

    Created a how to page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home/Home.vue'
+import About from './views/About/About.vue'
 import Create from './views/Create/Create.vue'
 import Join from './views/Join/Join.vue'
 import Lobby from './views/Lobby/Lobby.vue'
@@ -17,6 +18,11 @@ export default new Router({
 			path: '/',
 			name: 'home',
 			component: Home
+		},
+		{
+			path: '/about',
+			name: 'about',
+			component: About
 		},
 		{
 			path: '/create',
---
+++ src/views/About/About.vue
@@ -0,0 +1,19 @@
+<template src="./About.html"></template>
+<style src="./About.styl" lang="styl"></style>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+	name: "about",
+	data: () => {
+		return {};
+	},
+	components: {},
+	mounted: function() {
+	},
+	methods: {
+	}
+};
+</script>
+
